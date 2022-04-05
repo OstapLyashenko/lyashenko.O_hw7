e4b2088c84ba731ec17b4db932e6681359fbaeff
Your Name <ostap.lyashenko25gmail.com>
2022-04-05 17:55:38 +0300

    rgb

--- autoload.php
+++ autoload.php
@@ -1,21 +1,25 @@
 <?php
+
 class Autoloader
 {
-public function addNamespace(string $prefix, string $dir)
-{
+    public function addNamespace(string $prefix, string $dir)
+    {
 
+    }
+
+    public function register()
+    {
+        spl_autoload_register(array($this, 'autoload'));
+    }
+
+    public function autoload($class)
+    {
+        var_dump($class);
+        exit;
+
+    }
 }
 
-public function register()
-{
-spl_autoload_register(array($this, 'autoload'));
-}
-
-public function autoload($class)
-{
-}
-}
-
-$autoloader = new Psr4AutoloaderClass();
+$autoloader = new Autoloader();
 $autoloader->addNamespace('Hillel', 'src');
 $autoloader->register();
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 $dbh = new PDO('mysql:host=mysql;port=3306;dbname=sandbox', 'root', 'secret');
 
-require_once 'autoload.php';
+require_once '../autoload.php';
 
 $user = new \Hillel\Models\User();
 $c1 = new \Hillel\ValueObjackts\RgbColor();
